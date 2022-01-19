f5895bc4f6d1aa6e0a8c7190b9951859ccba6138
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-01-19 15:11:27 +0200

    Начало проекта

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+// const key = '25315376-9146c2ed2421f32b20fc9f298'
+
+// [
+//   {
+//       "id": 195893,
+//       "webformatURL": "https://pixabay.com/get/35bbf209e13e39d2_640.jpg",
+//       "largeImageURL": "https://pixabay.com/get/ed6a99fd0a76647_1280.jpg",
+//   },
+// ]
+
+function App() {
+  return (
+    <p>111</p>    
+  );
+}
+
+export default App;
