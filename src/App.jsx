e59a5783046559b8c18f02dc98a5bc5bb9a498af
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-01-29 18:41:59 +0200

    Выполнение ДЗ

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,113 @@
-import './App.css';
+import React, { Component } from 'react';
+import fetchImages from "./Services/api";
+import Searchbar from './Components/Searchbar/Searchbar';
+import ImageGallery from "./Components/ImageGallery/ImageGallery";
+import LoadMoreButton from "./Components/Button/Button";
+import Modal from './Components/Modal/Modal';
+import Loader from './Components/Loader/Loader';
 
-// const key = '25315376-9146c2ed2421f32b20fc9f298'
 
-// [
-//   {
-//       "id": 195893,
-//       "webformatURL": "https://pixabay.com/get/35bbf209e13e39d2_640.jpg",
-//       "largeImageURL": "https://pixabay.com/get/ed6a99fd0a76647_1280.jpg",
-//   },
-// ]
+class App extends Component { 
 
-function App() {
-  return (
-    <p>111</p>    
-  );
+  state = {
+    searchImages: "",
+    images: [],
+    page: 1,
+
+    showModal: false,
+    loading: false,
+
+    modalImage: "",
+    modalAlt: "",         
+  };
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchImages !== this.state.searchImages) {
+      this.setState({ images: [] });
+      this.fetchPictures();
+    }
+
+    if (prevState.page !== this.state.page && this.state.page !== 1) {
+      this.fetchPictures();
+    }
+  };
+
+
+  fetchPictures = () => {
+    const { searchImages, page } = this.state;
+
+    this.setState({ loading: true });
+    
+    fetchImages(searchImages, page)
+      .then((res) => {
+        const images = res.data.hits.map(
+          ({ id, tags, webformatURL, largeImageURL }) => {
+            return { id, tags, webformatURL, largeImageURL, };
+          }
+        );
+
+        if (images.length === 0) {
+          this.setState({ loading: false });
+          alert(`Sorry, nothing found`);
+          return;
+        }
+
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...images],
+        }));
+        this.setState({ loading: false });
+      });
+  };
+
+  handleFormSubmit = searchImages => {
+    this.setState({ searchImages, page: 1 });
+  };
+
+  onLoadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+  setModal = (largeImageURL, tags ) => {
+    this.setState({ modalImage: largeImageURL });
+    this.setState({modalAlt: tags});
+    this.toggleModal();
+  };
+
+  
+  render() {
+    const { images, loading, showModal, modalImage, modalAlt } = this.state;
+    const { handleFormSubmit, onLoadMore, toggleModal, setModal } = this;
+      
+    return (
+      <div>
+        <Searchbar onSubmit={handleFormSubmit} />
+
+        {images.length > 0 && (
+          <ImageGallery setModal={setModal} images={images} />
+        )}
+
+        {loading ?
+          // (<p>Загрузка ...</p>) :
+          (<Loader/>) :
+          ( this.state.images.length > 0 &&
+            this.state.images.length % 12 === 0 &&
+            (<LoadMoreButton onClick={onLoadMore} />)
+          )
+        }
+
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <img src={modalImage} alt={modalAlt} />
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,7 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { createPortal } from "react-dom";
+import { ModaOverlay, ModalWindow } from './Modal.styled'
 
+const modalRoot = document.querySelector("#modal-root");
 
-{/* <div class="overlay">
-  <div class="modal">
-    <img src="" alt="" />
-  </div>
-</div> */}
+class Modal extends Component {
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);     
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown); 
+  };
+
+  handleKeyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropDown = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onClose();
+    }
+  };
+
+  static propTypes = {
+      onClose: PropTypes.func.isRequired,
+  };
+
+  render() {  
+    return createPortal(
+      <ModaOverlay onClick={this.handleBackdropDown}>
+        <ModalWindow>
+          {this.props.children}
+        </ModalWindow>
+      </ModaOverlay>,
+      modalRoot
+    );
+  }
+}
+export default Modal;
